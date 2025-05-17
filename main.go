01d90cf41a4a1b45d0a0090d8559ad7ff92ade16
Scott <67354101+scGetStuff@users.noreply.github.com>
2025-05-17 18:30:18 -0400

    CH1 code done; help and exit

--- main.go
+++ main.go
@@ -1,12 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+
+	initMap()
+
+	const prompt = "Pokedex > "
+	input := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(prompt)
+		if input.Scan() {
+			stuff := cleanInput(input.Text())
+			if len(stuff) < 1 {
+				continue
+			}
+			first := stuff[0]
+			// fmt.Printf("Your command was: %s\n", first)
+
+			command, ok := commands[first]
+			if !ok {
+				fmt.Println("Unknown command")
+				continue
+			}
+
+			err := command.callback()
+			if err != nil {
+				fmt.Print(err)
+			}
+		}
+	}
+
 }
 
 func cleanInput(text string) []string {
@@ -16,9 +55,7 @@ func cleanInput(text string) []string {
 
 	out := make([]string, 0, len(words))
 
-	// fmt.Println("")
 	for _, word := range words {
-		// fmt.Printf("'%s', ", word)
 		word = strings.TrimSpace(word)
 		if word != "" {
 			out = append(out, word)
@@ -26,4 +63,39 @@ func cleanInput(text string) []string {
 	}
 
 	return out
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for k, v := range commands {
+		fmt.Printf("%s: %s\n", k, v.description)
+	}
+
+	return nil
+}
+
+// could not do this at declaration, circular dependency
+func initMap() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
